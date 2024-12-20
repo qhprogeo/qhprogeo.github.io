d9e4173bc2703884cb1b003150bd09889661b6ff
witran <witran@users.noreply.github.com>
2024-12-20 16:06:28 +0000

    deploy: 7c561024e72876efad192b719ee39848fee7b34f

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,61 +1,61 @@
 var store = [{
         "title": "Post: Modified Date",
-        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Blog"],
+        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["blogs"],
         "tags": ["Post Formats","readability","standard"],
         "url": "/blogs/post-modified/",
         "teaser": null
       },{
         "title": "Post: Standard",
-        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["Blog"],
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["blogs"],
         "tags": ["Post Formats","readability","standard"],
         "url": "/blogs/post-standard/",
         "teaser": null
       },{
+        "title": "Contest X, Problem 1",
+        "excerpt":"Given the function $f(x) = \\int_{0}^{x} \\frac{1}{1 + t^2} \\, dt$, find the value of $f’(x)$ and evaluate $f(\\pi)$. Additionally, consider the series $S = \\sum_{n=1}^{\\infty} \\frac{(-1)^{n+1}}{n}$. Determine whether the series converges or diverges. If it converges, find its sum. Finally, solve the following differential equation: $\\frac{d^2y}{dx^2} + 4\\frac{dy}{dx} +...","categories": ["problems"],
+        "tags": ["hard"],
+        "url": "/problems/2010/01/07/12357.html",
+        "teaser": null
+      },{
+        "title": "Contest X, Problem 2",
+        "excerpt":"Prove that the sum of the interior angles of a triangle is $180^\\circ$. Consider a triangle $ABC$. Extend the side $BC$ to a point $D$. Let $\\angle BAC = \\alpha$, $\\angle ABC = \\beta$, and $\\angle ACB = \\gamma$. By the exterior angle theorem, the exterior angle $\\angle ACD$ is...","categories": ["problems"],
+        "tags": [],
+        "url": "/problems/2010/01/07/12358.html",
+        "teaser": null
+      },{
+        "title": "Contest X, Problem 3",
+        "excerpt":"Consider the function $f: \\mathbb{R} \\to \\mathbb{R}$ that satisfies the following functional equation for all real numbers $x$ and $y$: \\[f(x + y) + f(x - y) = 2f(x)f(y)\\] Additionally, it is given that $f(0) = 1$. Prove that $f$ is an even function, i.e., $f(x) = f(-x)$ for all...","categories": ["problems"],
+        "tags": [],
+        "url": "/problems/2010/01/07/12359.html",
+        "teaser": null
+      },{
         "title": "Post: Chat",
-        "excerpt":"Abbott: Strange as it may seem, they give ball players nowadays very peculiar names. Costello: Funny names? Abbott: Nicknames, nicknames. Now, on the St. Louis team we have Who’s on first, What’s on second, I Don’t Know is on third– Costello: That’s what I want to find out. I want...","categories": ["Blog"],
+        "excerpt":"Abbott: Strange as it may seem, they give ball players nowadays very peculiar names. Costello: Funny names? Abbott: Nicknames, nicknames. Now, on the St. Louis team we have Who’s on first, What’s on second, I Don’t Know is on third– Costello: That’s what I want to find out. I want...","categories": ["blogs"],
         "tags": ["chat","Post Formats"],
         "url": "/blogs/post-chat/",
         "teaser": null
       },{
         "title": "Post: Notice",
-        "excerpt":"A notice displays information that explains nearby content. Often used to call attention to a particular detail. When using Kramdown {: .notice} can be added after a sentence to assign the .notice to the &lt;p&gt;&lt;/p&gt; element. Changes in Service: We just updated our privacy policy here to better service our...","categories": ["Blog"],
+        "excerpt":"A notice displays information that explains nearby content. Often used to call attention to a particular detail. When using Kramdown {: .notice} can be added after a sentence to assign the .notice to the &lt;p&gt;&lt;/p&gt; element. Changes in Service: We just updated our privacy policy here to better service our...","categories": ["blogs"],
         "tags": ["Post Formats","notice"],
         "url": "/blogs/post-notice/",
         "teaser": null
       },{
         "title": "Post: Quote",
-        "excerpt":"   Only one thing is impossible for God: To find any sense in any copyright law on the planet.       Mark Twain   ","categories": ["Blog"],
+        "excerpt":"   Only one thing is impossible for God: To find any sense in any copyright law on the planet.       Mark Twain   ","categories": ["blogs"],
         "tags": ["Post Formats","quote"],
         "url": "/blogs/post-quote/",
         "teaser": null
       },{
         "title": "Post: Link",
-        "excerpt":"This theme supports link posts, made famous by John Gruber. To use, just add link: http://url-you-want-linked to the post’s YAML front matter and you’re done.      And this is how a quote looks.    Some link can also be shown.  ","categories": ["Blog"],
+        "excerpt":"This theme supports link posts, made famous by John Gruber. To use, just add link: http://url-you-want-linked to the post’s YAML front matter and you’re done.      And this is how a quote looks.    Some link can also be shown.  ","categories": ["blogs"],
         "tags": ["link","Post Formats"],
         "url": "/blogs/post-link/",
         "teaser": null
       },{
         "title": "Blog cua Hung",
-        "excerpt":"Bold Italic Link You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates...","categories": ["blog"],
+        "excerpt":"Bold Italic Link You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates...","categories": ["blogs"],
         "tags": ["Jekyll","update"],
         "url": "/blogs/welcome-to-jekyll/",
-        "teaser": null
-      },{
-        "title": "12357",
-        "excerpt":"Given the function $f(x) = \\int_{0}^{x} \\frac{1}{1 + t^2} \\, dt$, find the value of $f’(x)$ and evaluate $f(\\pi)$. Additionally, consider the series $S = \\sum_{n=1}^{\\infty} \\frac{(-1)^{n+1}}{n}$. Determine whether the series converges or diverges. If it converges, find its sum. Finally, solve the following differential equation: $\\frac{d^2y}{dx^2} + 4\\frac{dy}{dx} +...","categories": [],
-        "tags": [],
-        "url": "/problems/12357.html",
-        "teaser": null
-      },{
-        "title": "12358",
-        "excerpt":"Prove that the sum of the interior angles of a triangle is $180^\\circ$. Consider a triangle $ABC$. Extend the side $BC$ to a point $D$. Let $\\angle BAC = \\alpha$, $\\angle ABC = \\beta$, and $\\angle ACB = \\gamma$. By the exterior angle theorem, the exterior angle $\\angle ACD$ is...","categories": [],
-        "tags": [],
-        "url": "/problems/12358.html",
-        "teaser": null
-      },{
-        "title": "12359",
-        "excerpt":"Consider the function $f: \\mathbb{R} \\to \\mathbb{R}$ that satisfies the following functional equation for all real numbers $x$ and $y$: \\[f(x + y) + f(x - y) = 2f(x)f(y)\\] Additionally, it is given that $f(0) = 1$. Prove that $f$ is an even function, i.e., $f(x) = f(-x)$ for all...","categories": [],
-        "tags": [],
-        "url": "/problems/12359.html",
         "teaser": null
       }]
